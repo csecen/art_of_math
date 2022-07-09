66351ec2a88efb27a7023a57670c10412304db39
csecen <csecen@verizon.net>
2022-07-09 09:08:27 -0400

    Comments added to collatz conjecture py file. Addition of outputing image file to directory named images

--- collatz_conjecture/collatz.py
+++ collatz_conjecture/collatz.py
@@ -15,16 +15,22 @@ class Collatz:
         '''
         This class represents a node in the tree that can be formed by following
         the collatz conjecture.
+        
+        Parameters:
+            value --> the current value of the node
         '''
-        def __init__(self, value):
+        def __init__(self, value: int):
             self.value = value
             self.children = []
     
     
-    def __init__(self, value):
+    def __init__(self, value: int):
         '''
         Initialize the root node of the tree and build the tree out for the
         numbers specified by the user.
+        
+        Parameters:
+            value --> the value of the root node
         '''
         self.node = self.Node(None)
         nodes = {}
@@ -33,7 +39,7 @@ class Collatz:
             _ = self.collatz(self.node, i, nodes)
     
     
-    def collatz(self, node, n, nodes):
+    def collatz(self, node: Node, n: int, nodes: dict):
         '''
         Follow the rules of the collatz conjecture to build out a tree that
         represents the numbers obtained. The collatz conjecture is simple:
@@ -44,6 +50,16 @@ class Collatz:
         The conjecture states that any number chosen will eventually reach
         the terminal loop 4 --> 2 --> 1 --> 4...
         These simple rules lead to a complex tree.
+        
+        Parameters:
+            node  --> root node of the tree
+            n     --> current value being added to the tree
+            nodes --> dictionary containing the nodes that have already been
+                      added to the tree
+                      
+        Returns:
+            root     --> the root node of the tree
+            new_node --> node that was just created
         '''
         # if the node has already bee found return it so as to not
         # traverse the same path twice
@@ -71,10 +87,13 @@ class Collatz:
             return root, new_node
             
             
-    def draw_collatz(self, tree, tab):
+    def draw_collatz(self, tree: Node):
         '''
         Using the python turtle module, this method draws the collatz 
         conjecture for the numbers that were already calculated.
+        
+        Parameters:
+            tree --> the tree that the nodes will be added to
         '''
         pos = turtle.pos()   # save position in case of branching
         heading = turtle.heading()   # save heading in case of branching
@@ -90,7 +109,7 @@ class Collatz:
             return
         else:
             for child in tree.children:
-                self.draw_collatz(child, tab)
+                self.draw_collatz(child)
                 # reset the starting position after a branching occured
                 turtle.pu()
                 turtle.goto(pos)
@@ -104,17 +123,26 @@ class Collatz:
         Set the background of the image and draw the collatz conjecture
         according to the parameters passed in by the user. Optionally 
         saves the image to a file.
+        
+        Parameters:
+            bg             --> background color of image
+            lc             --> line color of image
+            size           --> size of the image
+            starting_point --> starting point of the image
+            filename       --> Optional: filename of the image file. If not
+                               provided, image will not be saved.
         '''
         setup_background(size, bg, starting_point)
         turtle.pencolor(lc)
-        self.draw_collatz(self.node, '')
+        self.draw_collatz(self.node)
 
         turtle.update()
         
         if filename:
             turtle.getscreen().getcanvas().postscript(file= filename+'.eps', colormode='color')
-            img = Image.open(filename + '.eps') 
-            img.save(filename + '.jpg')
+            img = Image.open(filename + '.eps')
+            if not os.path.exists('./images'): os.makedirs('./images')
+            img.save(f'images/{filename}.jpg')
             img.close()
             os.remove(f'{filename}.eps')
             
