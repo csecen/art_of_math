eb24a9937ed2b3adbabcd2af90ba1d83b5e82bcf
connor <secen.0609@gmail.com>
2024-04-28 10:02:42 -0400

    Update to naming convention for files and addition of ulam spiral

--- primes/twin_primes_graph.py
+++ primes/twin_primes_graph.py
@@ -10,22 +10,13 @@ tps = \
  (347, 349), (419, 421), (431, 433), (461, 463), (521, 523), (569, 571), (599, 601), (617, 619), (641, 643), (659, 661),
  (809, 811), (821, 823), (827, 829), (857, 859), (881, 883), (1019, 1021)]
 
-def main():
-    parser = argparse.ArgumentParser(description='Procude graph depicting the twin primes.')
-    parser.add_argument('size', metavar='s', nargs=2, type=int,
-                        help='the size of the graph/image')
-    parser.add_argument('background', metavar='b', type=str,
-                        help='the background color of the graph/image')
-    parser.add_argument('cmap', metavar='c', type=str,
-                        help='the colormap used for the graph/image')
-    parser.add_argument('filename', metavar='f', nargs='?', type=str,
-                        help='(optional) the filename of where to save the image')
-    args = parser.parse_args()
-    
-    size = tuple(vars(args)['size'])
-    background = vars(args)['background']
-    cmap_name = vars(args)['cmap']
-    filename = vars(args)['filename']
+
+def produce_image(params):
+    size = tuple(params['size'])
+    background = params['background']
+    cmap_name = params['cmap']
+    filename = params['filename']
+    show = params['show']
     
     # list of linearly spaced points being applied to function to for a line
     x = np.linspace(-20,20,100)
@@ -54,13 +45,12 @@ def main():
         m = (t[0]+1)/6
         y = (x/m)+(6*m)
         plt.plot(x,y, color=colors[i])
-        
-        
+
     # optionally save and show the plot
     plt.xlim([-5, 200000])
     plt.ylim([0, 3000])
     if filename:
         plt.savefig(filename, bbox_inches='tight', pad_inches=0)
-    plt.show()
-    
-main()
+
+    if show:
+        plt.show()
---
+++ primes/ulam_spiral.py
@@ -0,0 +1,107 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from utils import compute_primes as cp
+from functools import reduce
+
+
+def factors(n):
+    return set(reduce(list.__add__,
+                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
+
+def produce_image(params):
+
+    n = params['n']
+    size = tuple(params['size'])
+    background = params['background']
+    cmap_name = params['cmap_name']
+    colors = params['colors']
+    filename = params['filename']
+    show = params['show']
+    primes_only = params['primes_only']
+
+    up = False
+    down = False
+    left = False
+    right = True
+    hor = temp_hor = 1
+    ver = temp_ver = 1
+
+    x = y = 0
+    primes = set(cp.primesfrom2to(n))
+    data = np.zeros((n, 4))
+
+    for i in range(n):
+
+        data[i][0] = x
+        data[i][1] = y
+
+        if (i+1) in primes:
+            data[i][2] = 1
+            data[i][3] = 5
+        else:
+            data[i][2] = 0
+            data[i][3] = .5*len(factors(i+1))
+
+        if right:
+            x += 1
+            hor -= 1
+            if hor == 0:
+                temp_hor += 1
+                hor = temp_hor
+                up = True
+                right = False
+        elif up:
+            y += 1
+            ver -= 1
+            if ver == 0:
+                temp_ver += 1
+                ver = temp_ver
+                left = True
+                up = False
+        elif left:
+            x -= 1
+            hor -= 1
+            if hor == 0:
+                temp_hor += 1
+                hor = temp_hor
+                down = True
+                left = False
+        elif down:
+            y -= 1
+            ver -= 1
+            if ver == 0:
+                temp_ver += 1
+                ver = temp_ver
+                right = True
+                down = False
+
+    fig, ax = plt.subplots()
+    fig.set_size_inches(size)
+    fig.patch.set_visible(True)
+    fig.patch.set_facecolor(background)
+    ax.grid(False)
+
+    # plt.scatter(data[:, 0], data[:, 1], c=data[:, 2], s=data[:, 3], cmap=cmap_name)
+    if primes_only:
+        mask = data[:, 2] == 1
+        if colors:
+            plt.scatter(data[mask, 0], data[mask, 1], c=colors, s=data[mask, 3])
+        else:
+            plt.scatter(data[mask, 0], data[mask, 1], c=data[mask, 0], s=data[mask, 3], cmap=cmap_name)
+    else:
+        if colors:
+            mask = data[:, 2] == 0
+            plt.scatter(data[mask, 0], data[mask, 1], c=colors[0], s=data[mask, 3])
+            plt.scatter(data[~mask, 0], data[~mask, 1], c=colors[1], s=data[~mask, 3])
+        else:
+            plt.scatter(data[:, 0], data[:, 1], c=data[:, 2], s=data[:, 3], cmap=cmap_name)
+
+    # plt.scatter(data[:, 0], data[:, 1], c=data[:, 2], s=data[:, 3])
+    ax.axis('off')
+
+    if filename:
+        plt.savefig(filename, bbox_inches='tight', pad_inches=0, dpi=400)
+
+    if show:
+        plt.show()
