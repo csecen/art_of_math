8c30f822ae3eab2192c47c66e6b29d8866088b89
csecen <csecen@verizon.net>
2024-05-10 17:53:02 -0400

    Updated roatation plots code to new file naming convention

--- rotation_plots/rotation_plots.py
+++ rotation_plots/rotation_plots.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from pathlib import Path
 
 
 def produce_image(params):
@@ -10,18 +11,20 @@ def produce_image(params):
     background = params['background']
     cmap_name = params['cmap_name']
     color = params['color']
-    filename = params['filename']
+    save = params['save']
     show = params['show']
     scale_perc = params['scale_perc']
     translate_amt = params['translate_amt']
     theta = params['theta']
     vary_size = params['vary_size']
+    graph_type = ''
 
     # generate random coordinate data
     xs = np.random.uniform(low=-10, high=10, size=n)
     ys = np.random.uniform(low=-10, high=10, size=n)
 
     if vary_size:
+        graph_type += "vary_size"
         s = np.random.uniform(low=5, high=20, size=n)
     else:
         s = 10
@@ -37,17 +40,22 @@ def produce_image(params):
     nys = np.array(ys, copy=True)
 
     if scale_perc:
+        graph_type += "scale"
         nxs = nxs*scale_perc
         nys = nys*scale_perc
 
     if translate_amt:
+        graph_type += "translate"
         nxs = nxs+translate_amt
         nys = nys+translate_amt
 
     if theta:
+        graph_type += "rotate"
         nxs = nxs * np.cos(np.deg2rad(theta)) - nys * np.sin(np.deg2rad(theta))
         nys = nxs * np.sin(np.deg2rad(theta)) + nys * np.cos(np.deg2rad(theta))
 
+    if scale_perc and translate_amt and theta:
+        graph_type = "all"
 
     if color:
         plt.scatter(xs, ys, c=color, s=s)
@@ -58,7 +66,11 @@ def produce_image(params):
 
     ax.axis('off')
 
-    if filename:
+    if save:
+        path = f"output/rotation_plots/{graph_type}/{params['size'][0]}x{params['size'][1]}/"
+        Path(path).mkdir(parents=True, exist_ok=True)
+
+        filename = path + f'{background}_{cmap_name if cmap_name else color}.png'
         plt.savefig(filename, bbox_inches='tight', pad_inches=0, dpi=400)
 
     if show:
